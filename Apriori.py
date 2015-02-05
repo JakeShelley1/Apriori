d3772a5c07f74eca059541ab197a78a4eb653f15
Jake <jakeshelley1@gmail.com>
2015-02-05 12:18:37 -0500

    createCandidate added

--- Apriori.py
+++ Apriori.py
@@ -5,25 +5,56 @@
 
 import numpy as np
 
+#create hashtable of size 'size'
 def createHashTable(size):
 	hashTable = {}
 	for i in range(size):
 		hashTable[i] = 0
 	return hashTable
 
+#return hash address for itemSet
 def hashItemSets(x, y):
-	itemSetHash[((x * 10) + y) % 7] += 1
+	return(((x * 10) + y) % 7)
 
-def parseLine(line):
+#create hash table 'L'
+def createL(line):
 	array = line.split(" ")
 	for i in range (len(array) - 1):
 		L[int(array[i])] += 1
 		for j in range (i + 1, len(array) - 1):
-			hashItemSets(int(array[i]), int(array[j]))
+			address = hashItemSets(int(array[i]), int(array[j]))
+			itemSetHash[address] += 1
 
-with open('T10I4D100KTest.dat') as data:
-	#Create hashes of estimated size
+#write candidate
+def createCandidate(line, minSupp, candidate):
+	array = line.split(" ")
+	for i in range (len(array) - 1):
+		if (L[int(array[i])] < minSupp):
+			continue
+		else:
+			for j in range (i + 1, len(array) - 1):
+				if (L[int(array[j])] >= minSupp):
+					address = hashItemSets(int(array[i]), int(array[j]))
+					if (itemSetHash[address] >= minSupp):
+						print(i, j)
+
+
+def apriori(inp, minSupp, out):
+	global itemSetHash, L
 	itemSetHash = createHashTable(2000)
 	L = createHashTable(980)
-	for line in data:
-		parseLine(line)
+	with open(inp) as data:
+		for line in data:
+			createL(line)
+
+	with open(inp) as data:
+		candidate = open(out, 'w')
+		for line in data:
+			print(line)
+			createCandidate(line, minSupp, candidate)
+
+
+apriori('T10I4D100KTest.dat', 2, 'candidate.dat')
+
+
+
