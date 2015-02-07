58e54b07422ee3cc68c16faefbc6f7b40941f0f1
Jake <jakeshelley1@gmail.com>
2015-02-07 10:33:19 -0500

    Prints unique numbers only once

--- Apriori.py
+++ Apriori.py
@@ -14,10 +14,7 @@ def createHashTable(size):
 		hashTable[i] = 0
 	return hashTable
 
-# #return hash address for pair of items
-# def hashItemPair(x, y):
-# 	return(((x * 10) + y) % 7)
-
+#hash function for 'itemSetHash'
 def getHashAddress(array):
 	k = int(array[0])*10
 	for i in array:
@@ -25,6 +22,7 @@ def getHashAddress(array):
 	k = k % 37173
 	return k
 		 
+#fill hashtables		 
 def createLAnditemSetHash(line):
 	array = line.split(" ")
 	for i in range (len(array) - 1):
@@ -42,7 +40,6 @@ def createCandidate(line, minSupp, candidate):
 		if (L[int(array[i])] < minSupp):
 			continue
 		else:
-			candidate.write(array[i] + " (" + str(L[int(array[i])]) + ")\n" )
 			for j in range (i + 1, len(array) - 1):
 				if (badValue):	#break if a value between i and j is not within minSupp
 					break
@@ -64,15 +61,22 @@ def createCandidate(line, minSupp, candidate):
 def apriori(inp, out, minSupp,):
 	global itemSetHash, L
 	itemSetHash = createHashTable(40000)	#This should be set to the estimated highest value of your hash table
-	L = createHashTable(2000)	#This should be set to the highest value in the file
+	L = createHashTable(1000)	#This should be set to the highest value in the file
 	with open(inp) as data:
 		for line in data:
 			createLAnditemSetHash(line)
 
+	candidate = open(out, 'w')
+	candidate.write("Unique numbers:\n\n")
+	for key in L:
+		if (L[key] > minSupp):
+			candidate.write(str(key) + " (" + str(L[key]) + ")\n" )
+
+	candidate.write("\n\nItem Sets:\n\n")
 	with open(inp) as data:
-		candidate = open(out, 'w')
 		for line in data:
 			createCandidate(line, minSupp, candidate)
+	candidate.close()
 
 def main():
 	inp = str(sys.argv[1])
@@ -83,4 +87,3 @@ def main():
 start_time = time.time()
 main()
 print(time.time() - start_time)
-print(itemSetHash[0])
