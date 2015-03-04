554b9278f9e8eaa9590e203df414d234fc71a120
Jake <jakeshelley1@gmail.com>
2015-03-03 23:53:05 -0500

    lower runtime

--- Apriori.py
+++ Apriori.py
@@ -3,75 +3,77 @@ import sys
 from collections import Counter
 from itertools import combinations
 
-#Counts all sets greather than 2 using combinations
-def countAllSets(array):
-	for i in range(3, len(array)):	#Set the second parameter to the max size of itemsets you want to test (set to len(array) to analyze the entire array)
-		for j in combinations(array, i):
-			y = list(j)
-			temp = " ".join(y)
-			count[temp] +=1
-
-#Count item sets with a size of 2
-def countPairs(i, array):
-	sliceArray = []
-	sliceArray.append(array[i])
-	for j in range(i+1, len(array)):
-		sliceArray.append(array[j])
-		temp = " ".join(sliceArray)
-		count[temp] += 1
-		sliceArray.remove(array[j])
-
-#Iterate throguh the line
-def countItemsAndPairs(array):
-	for i in range (len(array)):
-		count[array[i]] += 1
-		countPairs(i, array)	
-
 #Write the final candidate
-def writeCandidate(minSupp, candidate):
+def writeCandidate(out, minSupp):
 	for i in count:
 		if count[i] >= minSupp:
-			candidate.write(i + " (" + str(count[i]) + ")\n")
+			out.write(i + " (" + str(count[i]) + ")\n")
 
-def countSingles(array):
-	for i in range(len(array)):
-		singleCount[array[i]] += 1
+#Counts all sets greather than 2 using combinations
+def countAllSets(array, start):
+	for j in combinations(array, start):
+		y = list(j)
+		temp = " ".join(y)
+		count[temp] +=1			
 
-def itemsWorthChecking(array, minSupp):
-	tempArray = []
-	for i in array:
-		if (singleCount[i] >= minSupp):
-			tempArray.append(i)
-	return tempArray
+#Prune table for values and sets that no longer fit minimum support
+def pruneTable(table, start, minSupp):
+	for z in range((len(table))):
+		array = table[z]
+		deleteArray = []
+		for i in range(len(array)):
+			deleteArray.append(True)
+		for j in combinations(array, start):
+			y = list(j)
+			tempArray = " ".join(y)
+			if (count[tempArray] >= minSupp):
+				tempArray = tempArray.split(" ")
+				for item in tempArray:
+					deleteArray[array.index(item)] = False
+		deletion = []
+		for x in range(len(deleteArray)):
+			if (deleteArray[x]):
+				deletion.append(array[x])
+		for item in deletion:
+			array.remove(item)
+		table[z] = array	
+	return table
+
+#Create initial table
+def createDataTable(inp):
+	table = []
+	with open(inp) as data:
+		for line in data:
+			array = line.split(" ")
+			tempInt = len(array) - 1
+			if (array[tempInt] == '\n' or array[tempInt] == ''):
+				del array[tempInt]
+			array = map(int, array)
+			array.sort()
+			array = map(str, array)
+			table.append(array)
+	return table
 
 def apriori(inp, out, minSupp):
-	global count, singleCount
+	global count, singleCount, flag
 	count = Counter()
-	singleCount = Counter()
-	with open(inp) as data:
-		for line in data:
-			array = line.split(" ")
-			tempInt = len(array) - 1
-			if (array[tempInt] == '\n' or array[tempInt] == ''):
-				del array[tempInt]
-			countSingles(array)
-	lineCount = 0
-	with open(inp) as data:
-		for line in data:
-			lineCount += 1
-			array = line.split(" ")
-			tempInt = len(array) - 1
-			if (array[tempInt] == '\n' or array[tempInt] == ''):
-				del array[tempInt]
-			array.sort()
-			tempArray = itemsWorthChecking(array, minSupp) #countItemsAndPairs(array)
-			if (lineCount%1000 == 0):
-				print(time.time() - start_time)
-			countAllSets(tempArray)
-	candidate = open(out, 'w')
-	writeCandidate(minSupp, candidate)
-	candidate.close()
-	
+	flag = True
+	start = 1
+	table = createDataTable(inp)
+	while (flag):
+		flag = False
+		for array in table:
+			countAllSets(array, start)
+		table = pruneTable(table, start, minSupp)
+		start += 1
+		for check in table:
+			if len(check) != 0:
+				flag = True
+
+	out = open(out, 'w')
+	writeCandidate(out, minSupp)
+	out.close()
+
 def main():
 	inp = str(sys.argv[1])
 	out = str(sys.argv[2])
